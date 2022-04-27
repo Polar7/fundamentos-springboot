78b590bf1a2e08f3df7026115432807f8efb9cf1
Jean Lozano <michecoanexa52@gmail.com>
2022-04-26 23:34:33 -0500

    Initial config

---
+++ src/test/java/com/fundamentos/FundamentosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
